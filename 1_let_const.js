798cc05ec9986443f9a5bff847f956db78875b56
IRONKAGE <IRONKAGE@gmail.com>
2020-03-16 16:01:38 +0200

    add [let & const]

---
+++ 1_let_const.js
@@ -0,0 +1,60 @@
+// var num = 42
+// num = 'str'
+// console.log(num)
+
+
+// if (true) {
+//     var a = 42
+// } 
+// console.log(a)
+
+// let a = 24; 
+// if (true) {
+//     let a = 42
+//     console.log('a', a)
+// } 
+// console.log(a)
+
+
+
+
+
+// Hoisting
+// b = 20
+// console.log(b)
+// var b = 10
+
+// b = 20
+// console.log(b)
+// let b = 10
+
+
+// function hoisted() {
+//     age = 26
+// }
+// let age
+// hoisted()
+// console.log(age)
+
+
+
+
+
+// Const
+// const COLOR = '#ffeebb'
+// console.log(COLOR)
+
+// const COLOR = '#ffeebb'
+// COLOR = '#000000'
+// console.log(COLOR)
+
+
+// const array = [1, 2, 3, 4, 5]
+// console.log(array)
+// array.push(13)
+// console.log(array)
+
+
+// const obj = { a: 42}
+// obj.name = 'Oleh'
+// console.log(obj)
