3414fb8039d05a03b8bb10ffdcd3adb3b9d6ee2a
IRONKAGE <IRONKAGE@gmail.com>
2020-03-16 16:15:04 +0200

    add [modules]

---
+++ 7_modules/index.js
@@ -0,0 +1,13 @@
+// import Logger, {COLOR, compute} from './module'
+
+// compute(1, 2)
+// console.log(COLOR);
+// Logger.log()
+
+
+
+
+
+import * as Module from './module'
+Module.compute(1, 5)
+Module.default.log()
